b06f42dee45f96652db273a8d1eba60cd6509122
wangweiwei1 <wangweiwei1@xiaomi.com>
2015-02-10 17:31:17 +0800

    Modify the group script.
    N/A
    
    Change-Id: Ic8786c646e1befd579250a8a67958dd0a4e48ee9
    Signed-off-by: wangweiwei1 <wangweiwei1@xiaomi.com>

--- src/com/android/contacts/Test_T1755392_NewGroup.java
+++ src/com/android/contacts/Test_T1755392_NewGroup.java
@@ -33,38 +33,39 @@ public class Test_T1755392_NewGroup extends UiAutomatorTestCase {
 		mm.log("Step 1: Enter Contact app from Launcher by pressing Contact button.");
 		mm.pressHome();
 		mm.waitFor(1);
-		mm.getObjectByText("联系人", "android.widget.TextView").click();
+		mm.getObjectByText("鑱旂郴浜�", "android.widget.TextView").click();
 		mm.waitFor(1);
 		CheckCurrentView("com.android.contacts/com.android.contacts.activities.PeopleActivity");
 
 		mm.log("Step 2: Enter Group view by clicking Group item.");
-		mm.getObjectByText("我的群组", "android.widget.TextView").click();
+		mm.getObjectByText("鎴戠殑缇ょ粍", "android.widget.TextView").click();
 		mm.waitFor(1);
 		CheckCurrentView("com.android.contacts/com.android.contacts.activities.ContactsGroupActivity");
 
 		mm.log("Step 3: Click New Group button, input group name, and confirm.");
 		mm.log("Expected result: Group is new created successfully and list in Group list, group exists dailog pops if ");
 		mm.log("the group exists already.");
-		mm.getObjectByText("新建群组", "android.widget.Button").click();
+		mm.getObjectByText("鏂板缓缇ょ粍", "android.widget.Button").click();
 		mm.waitFor(1);
 		UiObject group_name = new UiObject(new UiSelector()
                 .className("android.widget.EditText").index(0));
-		group_name.setText(Utf7ImeHelper.e("群组1"));
-		//mm.shell("input text 群组1");
+		group_name.setText(Utf7ImeHelper.e("缇ょ粍1"));
+		//mm.shell("input text 缇ょ粍1");
 		mm.waitFor(1);
-		mm.getObjectByText("确定", "android.widget.Button").click();
+		mm.getObjectByText("纭畾", "android.widget.Button").click();
 		mm.waitFor(1);
 		mm.pressBack();
 		mm.waitFor(1);
-		cc.assertUiObejctExist(mm.getObjectByText("群组1", "android.widget.TextView"));
-		mm.getObjectByText("新建群组", "android.widget.Button").click();
+		cc.assertUiObejctExist(mm.getObjectByText("缇ょ粍1", "android.widget.TextView"));
+		mm.getObjectByText("鏂板缓缇ょ粍", "android.widget.Button").click();
 		mm.waitFor(1);
-		group_name.setText(Utf7ImeHelper.e("群组1"));
+		group_name.setText(Utf7ImeHelper.e("缇ょ粍1"));
 		mm.waitFor(1);
-		mm.getObjectByText("确定", "android.widget.Button").click();
+		mm.getObjectByText("纭畾", "android.widget.Button").click();
 		mm.waitFor(5);
-		//cc.assertTextExist("群组已存在");not able to recognize the toast text.
-		cc.assertUiObejctExist(mm.getObjectByText("确定", "android.widget.Button"));
+		mm.waitFor(1);
+		//cc.assertTextExist("缇ょ粍宸插瓨鍦�");not able to recognize the toast text.
+		cc.assertUiObejctExist(mm.getObjectByText("纭畾", "android.widget.Button"));
 	}
 
 	private boolean CheckCurrentView(String activityName) throws UiObjectNotFoundException{
@@ -77,12 +78,12 @@ public class Test_T1755392_NewGroup extends UiAutomatorTestCase {
 
 	@Override
 	protected void tearDown() throws Exception {
-		mm.getObjectByText("取消", "android.widget.Button").click();
-		mm.getObjectByText("群组1", "android.widget.TextView").click();
+		mm.getObjectByText("鍙栨秷", "android.widget.Button").click();
+		mm.getObjectByText("缇ょ粍1", "android.widget.TextView").click();
 		mm.waitFor(2);
-		mm.getObjectByText("删除", "android.widget.Button").click();
+		mm.getObjectByText("鍒犻櫎", "android.widget.Button").click();
 		mm.waitFor(1);
-		mm.getObjectByText("确定", "android.widget.Button").click();
+		mm.getObjectByText("纭畾", "android.widget.Button").click();
 		mm.waitFor(1);
 		mm.pressHome();
 		mm.waitFor(1);
